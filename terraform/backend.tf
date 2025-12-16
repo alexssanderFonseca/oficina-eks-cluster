0259c7688d1954a9296794e1d59d408ed51ff2ba
alexssanderFonseca <79030268+alexssanderFonseca@users.noreply.github.com>
2025-12-16 09:13:13 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "tfstate-fiap-alex-academy"
+    bucket  = "tfstate-fiap-alex-academy-eks"
     key     = "tfstate"
     region  = "us-east-1"
     encrypt = false
