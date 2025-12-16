bf65bef1e70ed7d7cdf70b98952d6d1394503c98
Alexssander Fonseca <alex.marques.fa@gmail.com>
2025-12-16 08:08:01 -0300

    fet: ajustes

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -4,7 +4,7 @@ resource "aws_eks_cluster" "eks-cluster" {
   role_arn = var.labRole
 
   vpc_config {
-    subnet_ids         = module.vpc.private_subnets  
+    subnet_ids         = module.vpc.private_subnets
     security_group_ids = [aws_security_group.sg.id]
   }
 
